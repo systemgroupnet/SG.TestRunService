62632fdf7b02ba12feff8ad8c24bd4d2827085f9
Mohammad Dehghan <mohammadhd@systemgroup.net>
2020-01-18 18:12:36 +0330

    `Attachment` should have a fk field to TestRunSession

--- SG.TestRunService/Data/Attachment.cs
+++ SG.TestRunService/Data/Attachment.cs
@@ -6,8 +6,11 @@ namespace SG.TestRunService.Data
     public class Attachment : IEntity
     {
         public int Id { get; set; }
+
         public int? TestRunId { get; set; }
         public int? TestCaseRunId { get; set; }
+        public int? TestRunSessionId { get; set; }
+
         public string Name { get; set; }
         public string Type { get; set; }
         public byte[] Data { get; set; }
