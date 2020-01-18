8671813e265b49bcf69ea28f3f1c9fa3627ccb5a
Mohammad Dehghan <mohammadhd@systemgroup.net>
2020-01-18 22:18:29 +0330

    What was that property?!

--- SG.TestRunService/Data/Attachment.cs
+++ SG.TestRunService/Data/Attachment.cs
@@ -8,7 +8,6 @@ namespace SG.TestRunService.Data
         public int Id { get; set; }
 
         public int? TestRunId { get; set; }
-        public int? TestCaseRunId { get; set; }
         public int? TestRunSessionId { get; set; }
 
         public string Name { get; set; }
