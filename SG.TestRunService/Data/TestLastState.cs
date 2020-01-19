8a3bab5078f603b4d38d791666a69572f994f6b3
Mohammad Dehghan <mohammadhd@systemgroup.net>
2020-01-19 04:33:25 +0330

    Make `TestLastState.LastOutcome` nullable
    Null means it has not ran yet and has no last outcome.

--- SG.TestRunService/Data/TestLastState.cs
+++ SG.TestRunService/Data/TestLastState.cs
@@ -17,7 +17,7 @@ namespace SG.TestRunService.Data
         public int ProductBuildInfoId { get; set; }
 
         public DateTime UpdateDate { get; set; }
-        public TestRunOutcome LastOutcome { get; set; }
+        public TestRunOutcome? LastOutcome { get; set; }
         public bool ShouldBeRun { get; set; }
         public RunReason? RunReason { get; set; }
 
