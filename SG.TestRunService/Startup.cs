dd0ca28d306ff376886bb8648459541f05df17a2
Mohammad Dehghan <mohammadhd@systemgroup.net>
2020-01-26 17:55:08 +0330

    Always use Developer Exception Page

--- SG.TestRunService/Startup.cs
+++ SG.TestRunService/Startup.cs
@@ -43,11 +43,7 @@ namespace SG.TestRunService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseDeveloperExceptionPage();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
