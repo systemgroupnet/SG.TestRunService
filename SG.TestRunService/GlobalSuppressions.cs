e7f1d44211155593fc9cca12ab4e24b18bdeb7e4
Mohammad Dehghan <mohammadhd@systemgroup.net>
2020-02-01 07:10:09 +0330

    Suppress CA messages

--- SG.TestRunService/GlobalSuppressions.cs
+++ SG.TestRunService/GlobalSuppressions.cs
@@ -4,3 +4,5 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:SG.TestRunService.Startup.Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder,Microsoft.AspNetCore.Hosting.IWebHostEnvironment)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1052:Static holder types should be Static or NotInheritable", Justification = "", Scope = "type", Target = "~T:SG.TestRunService.Program")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "May be required later!", Scope = "member", Target = "~M:SG.TestRunService.Startup.Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder,Microsoft.AspNetCore.Hosting.IWebHostEnvironment)")]
