70d19bcdf4d490c2d930b87b1c7f6d24c1a8e7d3
Mohammad Dehghan <mohammadhd@systemgroup.net>
2020-05-09 14:47:44 +0430

    WIP: Add integration tests project

---
+++ SG.TestRunService.IntegrationTests/TestingWebAppFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SG.TestRunService.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SG.TestRunService.IntegrationTests
+{
+    public class TestingWebAppFactory<T> : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(
+                      d => d.ServiceType ==
+                         typeof(DbContextOptions<TSDbContext>));
+
+                    if (descriptor != null)
+                    {
+                        services.Remove(descriptor);
+                    }
+
+                    services.AddDbContext<TSDbContext>(options =>
+                        options.UseInMemoryDatabase("InMemoryTestRunServiceDb"));
+
+                });
+        }
+    }
+}
