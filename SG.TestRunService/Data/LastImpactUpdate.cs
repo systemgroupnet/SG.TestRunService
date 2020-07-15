fd39d0aa587a8fd707473a08080ab230fe227ab7
Mohammad Dehghan <mohammadhd@systemgroup.net>
2020-07-15 17:24:43 +0430

    Remove unused "using"s

--- SG.TestRunService/Data/LastImpactUpdate.cs
+++ SG.TestRunService/Data/LastImpactUpdate.cs
@@ -1,8 +1,6 @@
 ﻿using SG.TestRunService.Infrastructure;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SG.TestRunService.Data
 {
